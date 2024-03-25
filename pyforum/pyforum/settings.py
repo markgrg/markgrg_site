e8d6b4007a0e623488c060acd7284e94eb9ca29e
Mark Grigorov <Grigorovmark7@gmail.com>
2024-03-25 14:19:52 +0300

    Update settings.py

--- pyforum/pyforum/settings.py
+++ pyforum/pyforum/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$fb99kpft8$t0o)0du%k)-8m*fagf#m3*p43)d*h=5-z*fw$hp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
