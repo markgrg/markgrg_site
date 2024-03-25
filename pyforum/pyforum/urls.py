9b3a82e7e094699e7e315b2b1e08d13356917b0f
Mark Grigorov <Grigorovmark7@gmail.com>
2024-03-25 22:50:27 +0300

    Update urls.py

--- pyforum/pyforum/urls.py
+++ pyforum/pyforum/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index), # 8000/
     path('cats/', views.categories), # /cats/
+    path('about/', views.about), # about/
 ]
 
 handler404 = views.page_not_found
