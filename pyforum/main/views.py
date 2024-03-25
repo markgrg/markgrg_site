dafca22ebb8f4f48304ffc5b9859af04fa23da41
Mark Grigorov <Grigorovmark7@gmail.com>
2024-03-25 22:50:24 +0300

    Update views.py

--- pyforum/main/views.py
+++ pyforum/main/views.py
@@ -2,11 +2,20 @@ from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
 def index(request):
-    return HttpResponse('Это страница MAIN')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+    }
+    return render(request, 'index_main.html', context=data) 
 
 def categories(request):
     return HttpResponse('Это страница CATS')
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+def about(request):
+    return render(request, 'about.html', {"title": 'О нас'})
